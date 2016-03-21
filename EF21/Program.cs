1b717db8a8a34883c84bdbf4fc5c2a7f914b709e
Waseem Soomro <ws@waseemsoomro.com>
2016-03-21 16:11:58 +0100

    some changes.

--- EF21/Program.cs
+++ EF21/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EF21.Models;
 
 namespace EF21
 {
@@ -10,6 +11,45 @@ namespace EF21
     {
         static void Main(string[] args)
         {
+            NORTHWNDEntities ne = new NORTHWNDEntities();
+
+            //// 
+            //// öVNINGAR 1
+            //// 
+            var orders = from m in ne.Orders  // tip: from m in ne.Orders.Skip(0).Take(10)
+                         select new
+                         {
+                             m.OrderDate,
+                             m.ShipName,
+                             m.Customer
+                         };
+            
+            foreach (var o in orders)
+            {
+                Console.WriteLine("{0} {1} {2}", o.OrderDate, o.ShipName, o.Customer.ContactName);
+            }
+
+            //// 
+            //// öVNINGAR 2
+            //// 
+            var categories = ne.Categories.OrderBy(c => c.CategoryName);
+
+            foreach (Category c in categories)
+            {
+                Console.WriteLine("{0} {1} {2}", c.CategoryName, c.Description, c.Products);
+            }
+
+            //// 
+            //// öVNINGAR 3
+            //// 
+
+
+            //// 
+            //// öVNINGAR
+            //// 
+
+
+            Console.ReadKey();
         }
     }
 }
