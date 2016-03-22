5d557e45d8d1bb1822304603ac061cda6f0379dc
Waseem Soomro <ws@waseemsoomro.com>
2016-03-22 17:16:09 +0100

    some more stuffs done.

--- EF21/Program.cs
+++ EF21/Program.cs
@@ -42,12 +42,80 @@ namespace EF21
             //// 
             //// öVNINGAR 3
             //// 
+            var categories2 = ne.Categories.OrderByDescending(c => c.CategoryName);
 
+            foreach (Category c in categories2)
+            {
+                Console.WriteLine("{0} {1} {2}", c.CategoryName, c.Description, c.Products);
+            }
+
+            //// 
+            //// öVNINGAR 4
+            //// 
+            var orders4 = from m in ne.Orders.Skip(10)
+                          select new
+                          {
+                              m.OrderDate,
+                              m.ShipName
+                          };
+
+            foreach (var o in orders4)
+            {
+                Console.WriteLine("{0} {1}", o.OrderDate, o.ShipName);
+            }
+
+
+            //// 
+            //// öVNINGAR 5
+            //// 
+
+            var orders5 = from m in ne.Orders.Where(m => m.CustomerID == "VINET")
+                          select new
+                          {
+                              m.OrderID,
+                              m.Customer,
+                              m.OrderDate,
+                              m.ShipName
+                          };
+            foreach (var o in orders5)
+            {
+                Console.WriteLine("{0} {1}", o.OrderDate, o.ShipName);
+            }
+
+            //// 
+            //// öVNINGAR 6
+            //// 
+            var orders6 = from m in ne.Orders.Where(m => m.EmployeeID == 5)
+                          select new
+                          {
+                              m.OrderID,
+                              m.Customer,
+                              m.OrderDate,
+                              m.ShipName
+                          };
+            foreach (var o in orders6)
+            {
+                Console.WriteLine("{0} {1}", o.OrderDate, o.ShipName);
+            }
+
+
+            //// 
+            //// öVNINGAR 7
+            //// 
+
+            var product_FirstOrDefault = ne.Products.FirstOrDefault();
+            Console.WriteLine(product_FirstOrDefault);
+            var product_First = ne.Products.First();
+            Console.WriteLine(product_First);
 
             //// 
             //// öVNINGAR
             //// 
 
+
+            //// 
+            //// öVNINGAR
+            //// 
 
             Console.ReadKey();
         }
